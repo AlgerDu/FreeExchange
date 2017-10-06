b4233810bc87cde587983d20089b76f69f558c16
Alger <AlgerDu@outlook.com>
2017-10-06 16:09:53 +0800

    接口定义

---
+++ D.FreeExchange.Core/Interface/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.FreeExchange.Core.Interface
+{
+    /// <summary>
+    /// 给 fitter 的命令类型
+    /// </summary>
+    public enum FitterCommand
+    {
+        Work,
+        Rest
+    }
+}
---
+++ D.FreeExchange.Core/Interface/IFitter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.FreeExchange.Core.Interface
+{
+    /// <summary>
+    /// 装配工
+    /// </summary>
+    public interface IFitter
+    {
+        /// <summary>
+        /// 装配（数据组包）
+        /// </summary>
+        /// <param name="product"></param>
+        void Installation(object product);
+
+        /// <summary>
+        /// 拆卸（数据分包）
+        /// </summary>
+        /// <param name="product"></param>
+        void Dismantling(object product);
+
+        /// <summary>
+        /// 执行命令
+        /// </summary>
+        /// <param name="command"></param>
+        void ExecuteCommand(FitterCommand command);
+    }
+}
