c24dcbf6485030b14d26ea8c6df41eb876b4c729
Alger <AlgerDu@outlook.com>
2017-10-08 02:28:47 +0800

    接口定义

---
+++ D.FreeExchange.Core/Interface/IActionCenter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.FreeExchange.Core.Interface
+{
+    /// <summary>
+    /// 接受到请求之后执行请求
+    /// 解析 url 和 params ，之后执行 action
+    /// 辅助接口，不是必需的
+    /// </summary>
+    public interface IActionCenter
+    {
+        /// <summary>
+        /// 执行请求，类 mvc
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        bool Execute(IContext context);
+    }
+}
--- D.FreeExchange.Core/Interface/IProductionLine.cs
+++ D.FreeExchange.Core/Interface/IProductionLine.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,5 +12,15 @@ namespace D.FreeExchange.Core.Interface
     /// </summary>
     public interface IProductionLine
     {
+        /// <summary>
+        /// 给生产线添加 socket 原料
+        /// 可以被重复调用，用于重连
+        /// </summary>
+        /// <param name="socket"></param>
+        void AddMaterial(Socket socket);
+
+        void Run();
+
+        void Close();
     }
 }
