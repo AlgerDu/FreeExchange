e7ae2c5200d280e05c846af4354099d708472565
Alger <AlgerDu@outlook.com>
2017-10-25 21:35:03 +0800

    类定义

---
+++ D.FreeExchange.Fitter.Young/Enums.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.FreeExchange.Fitter.Young
+{
+    /// <summary>
+    /// 包类型
+    /// </summary>
+    internal enum PackageCode
+    {
+        /// <summary>
+        /// 心跳
+        /// </summary>
+        Ping,
+
+        /// <summary>
+        /// 关闭
+        /// </summary>
+        Close,
+
+        /// <summary>
+        /// 文本
+        /// </summary>
+        Txt,
+
+        /// <summary>
+        /// 字节包描述
+        /// </summary>
+        ByteDescript,
+
+        /// <summary>
+        /// 字节
+        /// </summary>
+        Byte
+    }
+}
---
+++ D.FreeExchange.Fitter.Young/PackageFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.FreeExchange.Fitter.Young
+{
+    /// <summary>
+    /// 包生产
+    /// </summary>
+    internal class PackageFactory
+    {
+
+    }
+}
