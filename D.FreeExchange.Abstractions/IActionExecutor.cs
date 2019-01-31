7a4fb86a9d84e5abee34d8ba3f8346c9e0d97daa
Alger <AlgerDu@outlook.com>
2019-02-01 00:16:24 +0800

    添加 IActionExecutor 接口

---
+++ D.FreeExchange.Abstractions/IActionExecutor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.FreeExchange
+{
+    /// <summary>
+    /// action 执行器
+    /// </summary>
+    public interface IActionExecutor
+    {
+        /// <summary>
+        /// 执行 aciton
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        object InvokeAction(IExchangeMessage msg);
+    }
+}
