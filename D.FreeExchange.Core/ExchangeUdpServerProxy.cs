4de79c300f869e63a9afd66a06f9177536ca9f8a
Alger <AlgerDu@outlook.com>
2019-02-16 23:11:46 +0800

    需要有重大设计重组，先提交代码

---
+++ D.FreeExchange.Core/ExchangeUdpServerProxy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using D.Utils;
+using Microsoft.Extensions.Logging;
+
+namespace D.FreeExchange.Core
+{
+    public class ExchangeUdpServerProxy
+        : ExchangeProxy
+        , IExchangeServerProxy
+    {
+        public ExchangeUdpServerProxy(
+            ILogger logger
+            , string address
+            , IProtocolBuilder protocol
+            , IActionExecutor executor
+            ) : base(logger, address, protocol, executor)
+        {
+        }
+
+        public Task<IResult> Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult UpdateAddress(string newAddress)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
