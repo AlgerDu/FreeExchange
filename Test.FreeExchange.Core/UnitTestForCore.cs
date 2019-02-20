80117c0ed51698f8d5c5980c6d02f142d796c8b4
Alger <AlgerDu@outlook.com>
2019-02-20 20:49:16 +0800

    新建项目 Test.FreeExchange.Core
    用于已 Udp 相关实现来进行单元测试整体框架；以及整体框架使用的调整。

---
+++ Test.FreeExchange.Core/UnitTestForCore.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.FreeExchange.Core
+{
+    [TestClass]
+    public class UnitTestForCore
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
