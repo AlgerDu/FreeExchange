4ee5a6db6fd038e51955814782b75163d1d4824e
Alger <AlgerDu@outlook.com>
2019-02-23 23:03:10 +0800

    MvcActionExecutor 简单实现完成

--- D.FreeExchange.Core/MvcActionExecutor.cs
+++ D.FreeExchange.Core/MvcActionExecutor.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Text;
+using Autofac;
 using D.Utils;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
 
 namespace D.FreeExchange.Core
 {
@@ -27,23 +30,67 @@ namespace D.FreeExchange.Core
     {
         ILogger _logger;
         MvcActionExecutorOptions _options;
+        ILifetimeScope _scope;
 
         Dictionary<string, List<ActionItems>> _urlToActions;
 
         public MvcActionExecutor(
             ILogger<MvcActionExecutor> logger
             , IOptions<MvcActionExecutorOptions> options
+            , ILifetimeScope scope
             )
         {
             _logger = logger;
             _options = options.Value;
+            _scope = scope;
 
             AnaylseActions();
+            DiAllControllerType();
         }
 
         public IResult<object> InvokeAction(IExchangeMessage msg, IExchangeClientProxy clientProxy)
         {
-            throw new NotImplementedException();
+            var url = msg.Url;
+
+            var items = _urlToActions[url];
+
+            if (items == null)
+            {
+                return CreateError(ExchangeCode.ActionErrorType);
+            }
+
+            var paramsJson = msg.Params[0] as string;
+
+            ActionItems actionItem = null;
+            object[] actionParams = null;
+
+            foreach (var item in items)
+            {
+                var tryRst = TryResoleActionParams(item.Action.GetParameters(), paramsJson);
+
+                if (tryRst.IsSuccess())
+                {
+                    actionItem = item;
+                    actionParams = tryRst.Data;
+                    break;
+                }
+            }
+
+            if (actionItem != null)
+            {
+                var controller = _scope.Resolve(actionItem.ControllerType);
+
+                PreDealController(controller, clientProxy);
+
+                var actionRst = actionItem.Action.Invoke(controller, actionParams);
+
+                return Result.CreateSuccess<object>(actionRst);
+            }
+            else
+            {
+                return CreateError(ExchangeCode.ActionErrorType);
+            }
+
         }
 
         /// <summary>
@@ -61,15 +108,99 @@ namespace D.FreeExchange.Core
 
                 foreach (var type in assembly.GetTypes())
                 {
-                    if (type.IsPublic
-                        && type.IsClass
-                        && !type.IsAbstract
-                        && controllerType.IsAssignableFrom(type))
+                    if (IsOkClassType(type))
                     {
+                        foreach (var m in type.GetMethods())
+                        {
+                            if (m.IsPublic
+                                && !m.IsAbstract)
+                            {
+                                var item = new ActionItems
+                                {
+                                    Action = m,
+                                    ControllerType = type,
+                                    Url = $"{controllerType.Name}/{m.Name}"
+                                };
 
+                                if (_urlToActions.ContainsKey(item.Url))
+                                {
+                                    _urlToActions[item.Url].Add(item);
+                                }
+                                else
+                                {
+                                    _urlToActions.Add(item.Url, new List<ActionItems>() { item });
+                                }
+                            }
+                        }
                     }
                 }
             }
+        }
+
+        private bool IsOkClassType(Type ctlType)
+        {
+            var ctlInterface = typeof(IExchangeController);
+
+            if (ctlType.IsPublic
+                        && ctlType.IsClass
+                        && !ctlType.IsAbstract
+                        && ctlInterface.IsAssignableFrom(ctlType))
+            {
+                foreach (var namesapce in _options.Namespaces)
+                {
+                    if (ctlType.Namespace.StartsWith(namesapce))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private void DiAllControllerType()
+        {
+            _scope = _scope.BeginLifetimeScope((builder) =>
+            {
+                foreach (var items in _urlToActions.Values)
+                {
+                    foreach (var item in items)
+                    {
+                        builder.RegisterType(item.ControllerType);
+                    }
+                }
+            });
+        }
+
+        private IResult<Object> CreateError(ExchangeCode code)
+        {
+            return new Result<object>
+            {
+                Code = (int)code
+            };
+        }
+
+        private IResult<object[]> TryResoleActionParams(ParameterInfo[] parameters, string json)
+        {
+            var jarray = JArray.Parse(json);
+
+            List<object> rst = new List<object>();
+
+            for (var i = 0; i < parameters.Length; i++)
+            {
+                var ptype = parameters[i].ParameterType;
+                var jobject = jarray[i];
+
+                rst.Add(jobject.ToObject(ptype));
+            }
+
+            return Result.CreateSuccess<object[]>(rst.ToArray());
+        }
+
+        private void PreDealController(object controller, IExchangeClientProxy proxy)
+        {
+            var p = typeof(IExchangeController).GetProperty("Client");
+            p.SetValue(controller, proxy);
         }
     }
 }
