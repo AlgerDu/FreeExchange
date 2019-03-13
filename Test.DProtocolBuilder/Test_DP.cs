45a5aab26e86e2e3b2b64739a851c64b2c53d0e0
Alger <AlgerDu@outlook.com>
2019-03-13 22:11:21 +0800

    包更新前的提交

---
+++ Test.DProtocolBuilder/Test_DP.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using D.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using D.FreeExchange;
+
+namespace Test.DProtocolBuilder
+{
+    [TestClass]
+    public class Test_DP
+    {
+
+
+        private IContainer CreateContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.AddMicrosoftExtensions();
+
+            return builder.Build();
+        }
+    }
+
+    public class TestTaransporterForDP : ITransporter
+    {
+
+    }
+}
