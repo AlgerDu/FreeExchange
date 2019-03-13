7fd9d98e0bea2f4bf3a0681c904c60510cf6cb55
Alger <AlgerDu@outlook.com>
2019-03-13 21:46:37 +0800

    将一些常用的autofac 扩展，放置到一个单独的 dll 中

---
+++ Extensions/D.Extensions.Autofac.Microsoft/Extensions_Autofac_Microsoft.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace D.Extensions
+{
+    /// <summary>
+    /// 使用 autofac 时，有关 Microsoft.Extensions 的一些扩展
+    /// </summary>
+    public static class Extensions_Autofac_Microsoft
+    {
+        /// <summary>
+        /// 添加 Microsoft.Extensions 相关的一些组件
+        /// </summary>
+        /// <param name="builder"></param>
+        public static void AddMicrosoftExtensions(this ContainerBuilder builder)
+        {
+            var service = new ServiceCollection();
+            service.AddLogging();
+            service.AddOptions();
+
+            builder.Populate(service);
+        }
+    }
+}
