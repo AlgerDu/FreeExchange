8c1ae229138dc51943bd08f70f068d89bceea5d4
Alger <AlgerDu@outlook.com>
2019-03-23 22:51:16 +0800

    修改 Package 相关的单元测试

--- Test.DProtocolBuilder/Test_Package.cs
+++ Test.DProtocolBuilder/Test_Package.cs
@@ -10,38 +10,40 @@ namespace Test.DProtocolBuilder
         public void Test_Haad()
         {
             var p1 = new Package();
-            p1.Fin = true;
+            p1.Flag = FlagCode.End;
             p1.Code = PackageCode.Heart;
-            p1.Index = 15;
 
             var buffer = p1.ToBuffer();
+
+            Assert.AreEqual(buffer.Length, 1);
 
             var p2 = new Package();
             var index = 0;
             var need = p2.PushBuffer(buffer, ref index, buffer.Length);
 
             Assert.AreEqual(need, 0);
-            Assert.AreEqual(p1.Fin, p2.Fin);
+            Assert.AreEqual(p1.Flag, p2.Flag);
             Assert.AreEqual(p1.Code, p2.Code);
-            Assert.AreEqual(p1.Index, p2.Index);
         }
 
         [TestMethod]
         public void Test_Haad_BigIndex()
         {
             var p1 = new Package();
-            p1.Fin = true;
-            p1.Code = PackageCode.Disconnect;
-            p1.Index = 257;
+            p1.Flag = FlagCode.End;
+            p1.Code = PackageCode.Clean;
+            p1.Index = 256;
 
             var buffer = p1.ToBuffer();
+
+            Assert.AreEqual(buffer.Length, 3);
 
             var p2 = new Package();
             var index = 0;
             var need = p2.PushBuffer(buffer, ref index, buffer.Length);
 
             Assert.AreEqual(need, 0);
-            Assert.AreEqual(p1.Fin, p2.Fin);
+            Assert.AreEqual(p1.Flag, p2.Flag);
             Assert.AreEqual(p1.Code, p2.Code);
             Assert.AreEqual(p1.Index, p2.Index);
         }
