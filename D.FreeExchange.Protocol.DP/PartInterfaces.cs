a1b1bd5d0c9320abbe950f04143aee1f9fe9ebae
Alger <AlgerDu@outlook.com>
2019-04-05 21:27:40 +0800

    添加接口
    用来将 DProtocolBuilder 的逻辑进行合理的拆分；拆分出来的接口还有待调整。

---
+++ D.FreeExchange.Protocol.DP/PartInterfaces.cs
@@ -0,0 +1,50 @@
+﻿using D.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace D.FreeExchange.Protocol.DP
+{
+    internal interface IPackageInfo
+    {
+        IPackage Package { get; set; }
+
+        PackageState State { get; set; }
+    }
+
+    internal interface IShareData
+    {
+        bool BuilderIsRunning { get; set; }
+
+        ManualResetEvent MRE_ContinueSending { get; }
+
+        void SetOptions(DProtocolBuilderOptions options);
+
+        IReadOnlyDictionary<int, IPackageInfo> SendingPaks { get; }
+
+        IReadOnlyDictionary<int, IPackageInfo> ReceivingPaks { get; }
+    }
+
+    internal interface IPayloadAnalyser
+    {
+        IPackage[] Analyse(IProtocolPayload payload);
+    }
+
+    /// <summary>
+    /// 发送部分
+    /// </summary>
+    internal interface ISendPart
+    {
+        IResult SendCmdPakcage(IPackage package);
+
+        IResult SendPayloadPackages(IEnumerable<IPackage> packages);
+
+        void SetSendBufferAction();
+    }
+
+    internal interface IPackageFactory
+    {
+        IPackage CreatePackage(byte headBuffer);
+    }
+}
