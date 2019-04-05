6b2edea1cc0479f8de1ce44287ad04914b472eac
Alger <AlgerDu@outlook.com>
2019-04-05 22:46:32 +0800

    IPackageFactory 实现

---
+++ D.FreeExchange.Protocol.DP/PackageFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.FreeExchange.Protocol.DP
+{
+    internal class PackageFactory : IPackageFactory
+    {
+        public IPackage CreatePackage(byte headBuffer)
+        {
+            var header = new PackageHeader(headBuffer);
+
+            switch (header.Code)
+            {
+                case PackageCode.Connect:
+                    return new ConnectPackage(header);
+                case PackageCode.Heart:
+                    return new HeartPackage();
+                case PackageCode.Disconnect:
+                    return new DisconnectPackage();
+
+                case PackageCode.Clean:
+                case PackageCode.CleanUp:
+                case PackageCode.Answer:
+                case PackageCode.Lost:
+                    return new PackageWithIndex(header);
+
+                case PackageCode.Text:
+                case PackageCode.ByteDescription:
+                case PackageCode.Byte:
+                    return new ConnectPackage(header);
+
+                default:
+                    throw new Exception($"不支持的 package code {header.Code}");
+            }
+        }
+    }
+}
