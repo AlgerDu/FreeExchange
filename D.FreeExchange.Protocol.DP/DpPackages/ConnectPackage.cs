cdecd212327bae3a7424d6a3b633d8259da65b92
Alger <AlgerDu@outlook.com>
2019-07-31 09:01:45 +0800

    ConnectPackage 增加一个数据模型

--- D.FreeExchange.Protocol.DP/DpPackages/ConnectPackage.cs
+++ D.FreeExchange.Protocol.DP/DpPackages/ConnectPackage.cs
@@ -6,19 +6,39 @@ using System.Text;
 namespace D.FreeExchange.Protocol.DP
 {
     /// <summary>
-    /// 首次连接的连接包
+    /// 连接包携带的数据
+    /// </summary>
+    internal class ConnectPackageData
+    {
+        /// <summary>
+        /// 自定义唯一标识
+        /// 可以用来区分重启和重新连接等等
+        /// </summary>
+        public Guid Uid { get; set; }
+
+        /// <summary>
+        /// 可选配置参数
+        /// </summary>
+        public DProtocolOptions Options { get; set; }
+    }
+
+    /// <summary>
+    /// 连接包
     /// </summary>
     internal class ConnectPackage : PackageWithPayload
     {
         private Encoding _encoding;
 
-        public DProtocolOptions DpOptions
+        /// <summary>
+        /// 首次连接，客户端将配置传送给服务端
+        /// </summary>
+        public ConnectPackageData Data
         {
             get
             {
                 var json = _encoding.GetString(Payload);
 
-                return JsonConvert.DeserializeObject<DProtocolOptions>(json);
+                return JsonConvert.DeserializeObject<ConnectPackageData>(json);
             }
             set
             {
