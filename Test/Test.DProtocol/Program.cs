f9942aaf149cb14292159a3dfba9208d5ab3de76
杜智伟 <AlgerDu@outlook.com>
2019-08-06 23:12:42 +0800

    创建一个新的项目，用来测试自定义协议 DP

---
+++ Test/Test.DProtocol/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test.DProtocol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
