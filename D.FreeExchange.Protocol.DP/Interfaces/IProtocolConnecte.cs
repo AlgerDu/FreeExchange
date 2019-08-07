9284745c64aa76eb08b6931b82f155a68fdc9b75
杜智伟 <AlgerDu@outlook.com>
2019-08-07 16:52:30 +0800

    添加新的接口 IProtocolConnecte
    将连接相关的代码提炼出来，和心跳分离

---
+++ D.FreeExchange.Protocol.DP/Interfaces/IProtocolConnecte.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.FreeExchange.Protocol.DP
+{
+    /// <summary>
+    /// 连接相关
+    /// </summary>
+    public interface IProtocolConnecte
+    {
+        /// <summary>
+        /// 处理连接相关的协议包
+        /// </summary>
+        /// <param name="package"></param>
+        void DealPackage(IPackage package);
+    }
+}
