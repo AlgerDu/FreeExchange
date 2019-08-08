441cccb4e447e01955104b97b1a1314c0280b699
杜智伟 <AlgerDu@outlook.com>
2019-08-08 18:06:08 +0800

    添加项目，开始尝试编写 example

---
+++ Example/LiveChat/D.Example.LiveChat.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace D.Example.LiveChat.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
