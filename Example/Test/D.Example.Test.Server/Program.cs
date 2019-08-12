8047b91a97acc48177c7ddb5bbb9a470845e64e7
杜智伟 <AlgerDu@outlook.com>
2019-08-12 18:12:22 +0800

    添加两个新的 example 项目

---
+++ Example/Test/D.Example.Test.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace D.Example.Test.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Test/D.Example.Test.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace D.Example.Test.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
