d41dd3bcc01e03f95b772730fd78f2eae5ec7381
杜智伟 <AlgerDu@outlook.com>
2019-09-08 11:53:00 +0800

    IExchangeProxy 添加 4 个基本事件

--- D.FreeExchange.Abstractions/IExchangeProxy.cs
+++ D.FreeExchange.Abstractions/IExchangeProxy.cs
@@ -7,6 +7,34 @@ using System.Threading.Tasks;
 namespace D.FreeExchange
 {
     /// <summary>
+    /// ExchangeProxy 恢复在线
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    public delegate void ExchangeProxyBackOnlineEventHandler(object sender, ExchangeProxyEventArgs e);
+
+    /// <summary>
+    /// ExchangeProxy 离线
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    public delegate void ExchangeProxyOfflineEventHandler(object sender, ExchangeProxyEventArgs e);
+
+    /// <summary>
+    /// ExchangeProxy 连接成功
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    public delegate void ExchangeProxyConnectedEventHandler(object sender, ExchangeProxyEventArgs e);
+
+    /// <summary>
+    /// ExchangeProxy 即将关闭
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    public delegate void ExchangeProxyClosingEventHandler(object sender, ExchangeProxyEventArgs e);
+
+    /// <summary>
     /// 交换代理
     /// </summary>
     public interface IExchangeProxy
@@ -25,6 +53,11 @@ namespace D.FreeExchange
         /// 地址
         /// </summary>
         string Address { get; }
+
+        event ExchangeProxyBackOnlineEventHandler BackOnline;
+        event ExchangeProxyOfflineEventHandler Offline;
+        event ExchangeProxyConnectedEventHandler Connected;
+        event ExchangeProxyClosingEventHandler Closing;
 
         /// <summary>
         /// 主动断开与远程的连接
---
+++ D.FreeExchange.Abstractions/Models/ExchangeProxyEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.FreeExchange
+{
+    /// <summary>
+    /// ExchangeProxy 通用事件参数
+    /// </summary>
+    public class ExchangeProxyEventArgs
+    {
+        public DateTimeOffset Timestamp { get; set; }
+    }
+}
